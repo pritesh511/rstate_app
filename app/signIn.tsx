1af850224116cf1b209975216878f3da861618f2
Pritesh_Administer <apple_macbook_pro@Apple-MBPs-MBP.lan>
2024-12-23 13:32:49 +0530

    setup tailwind and nativewind for app

--- app/signIn.tsx
+++ app/signIn.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { View, Text } from "react-native";
 
 const SignIn = () => {
-    return(
-        <View>
-            <Text>This is sign in page</Text> 
-        </View>
-    )
+  return (
+    <View>
+      <Text>This is sign in page</Text>
+    </View>
+  );
 };
 
 export default SignIn;
